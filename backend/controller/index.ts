af41368a54eca7f632244e3fc0df014fd598178b
Noah Im <noah.im@sendbird.com>
2022-06-10 00:00:02 +0900

    fix : remove controller

--- backend/controller/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as cafeteriaController } from './cafeteria';
-export { default as cafeteriaMenuController } from './cafeteriaMenu';
-export { default as categoryController } from './category';
-export { default as orderController } from './order';
-export { default as orderDetailController } from './orderDetail';
-export { default as userController } from './user';
-export { default as documentController } from './document';
--- backend/controller/type.ts
+++
@@ -1,7 +0,0 @@
-export type Methods = {
-  [key in string]: Function;
-  // GET: Function;
-  // POST: Function;
-  // PATCH: Function;
-  // DELETE: Function;
-};
